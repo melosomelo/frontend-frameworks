f75e8940f086f307764f621eaa4b266270f2edc4
melosomelo <mateusmelornrn@gmail.com>
2020-09-25 12:29:31 -0300

    Setup firebase

---
+++ vue-project/src/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+// configs provided by firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAkGOT9qTy73w9d0JgaXN3iZJFo41d3cvI",
+  authDomain: "frontend-frameworks.firebaseapp.com",
+  databaseURL: "https://frontend-frameworks.firebaseio.com",
+  projectId: "frontend-frameworks",
+  storageBucket: "frontend-frameworks.appspot.com",
+  messagingSenderId: "300698978033",
+  appId: "1:300698978033:web:4c8050f8e4d88023080b50",
+};
+
+// initiailizing app
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export { db };
