e16229840666f3f964f15c4bc79ea67873886ed8
melosomelo <mateusmelornrn@gmail.com>
2020-09-30 18:17:34 -0300

    Implement no posts message in Home view

--- svelte-project/src/views/Home.svelte
+++ svelte-project/src/views/Home.svelte
@@ -1,9 +1,34 @@
+<script>
+  import { onMount } from "svelte";
+  onMount(async () => {
+    console.log("eai");
+  });
+
+  let posts = [];
+</script>
+
 <style>
   main {
     padding-top: 5rem;
   }
+
+  .no-posts-message {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .no-posts-message p {
+    text-align: center;
+  }
 </style>
 
 <main>
-  <h1>this is home!</h1>
+  {#if posts.length === 0}
+    <div class="no-posts-message">
+      <p style="margin-bottom: 1.5rem;">There are not posts here.</p>
+      <p><span>Why don't you write one?</span></p>
+    </div>
+  {/if}
 </main>
